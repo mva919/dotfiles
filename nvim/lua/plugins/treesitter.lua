25d3fbe94411a7042674ae09943992f222f39393
Marcos Villanueva <mva919@gmail.com>
2025-02-15 13:20:30 -0800

    Clean up plugins for nvim

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,21 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			ensure_installed = { "lua" },
-			auto_install = true,
-			highlight = { enabled = true },
-			indent = { enabled = true },
-		})
-	end,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				ensure_installed = { "lua" },
+				auto_install = true,
+				highlight = { enabled = true },
+				indent = { enabled = true },
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/playground",
+		config = function()
+			vim.keymap.set("n", "<leader>tsp", "<Cmd>TSPlayground<CR>", { desc = "Toggle TSPlayground" })
+		end,
+	},
 }
